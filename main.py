1692849753a3253555ff9e78c9f5582190698ddb
nyambayar1231 <nyambayarlucky@gmail.com>
2024-08-21 18:36:35 +0800

    Created pygame Clock object and using tick method

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill("black")
         pygame.display.flip()
+        elapsed = clock.tick(60)
+        dt = elapsed / 1000
+
 
 if __name__ == "__main__":
     main()
