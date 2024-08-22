0290a251fc6e74b874fe1d0fc16517c9967f7cc0
nyambayar1231 <nyambayarlucky@gmail.com>
2024-08-22 11:36:02 +0800

    Move player around right and left

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
+
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        elapsed = clock.tick(60)
-        dt = elapsed / 1000
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+        
 
 if __name__ == "__main__":
     main()
